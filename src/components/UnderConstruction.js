d725c721c89c7c1fcdb230659b4260101c0e830a
Emily Harber <emily.a.harber@gmail.com>
2020-05-28 22:22:02 -0400

    move under construction page to a component

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default () => {
+  return (
+    <div id="under-construction">
+      <h1>Hello World!</h1>
+      <h2>
+        Welcome to <a href="/">TheCodePixi.dev</a>
+      </h2>
+      <h3>this site is currently under construction...</h3>
+      <h4>
+        However, you can still view my <a href="/blog">Blog</a>
+        <br />
+        or follow me on{" "}
+        <a href="https://www.twitter.com/thecodepixi">Twitter</a> for regular
+        updates
+        <br />
+        Thanks for stopping by!
+      </h4>
+    </div>
+  )
+}
