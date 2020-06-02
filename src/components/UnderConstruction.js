226aafa4baf1fd12ab9a311c6154fd5041ce985a
Emily Harber <emily.a.harber@gmail.com>
2020-06-01 20:56:21 -0400

    styling changes to main page and blog theme

--- src/components/UnderConstruction.js
+++ src/components/UnderConstruction.js
@@ -3,25 +3,27 @@ import React from "react"
 export default () => {
   return (
     <div id="under-construction">
-      <div id="text">
-        <h1>Hello World!</h1>
-        <h2>
-          Welcome to <a href="/">TheCodePixi.dev</a>
-        </h2>
-        <h3>this site is currently under construction...</h3>
-        <h3>
-          However, you can still view my <a href="/blog">Blog</a>
-          <br />
-          or follow me on{" "}
-          <a href="https://www.twitter.com/thecodepixi">Twitter</a> for regular
-          updates.
-        </h3>
-        <h3>
-          Thanks for stopping by!{" "}
-          <span role="img" aria-label="waving hand emoji">
-            👋
-          </span>
-        </h3>
+      <div id="text-box">
+        <div id="text-background">
+          <h1>Hello World!</h1>
+          <h2>
+            Welcome to <a href="/">TheCodePixi.dev</a>
+          </h2>
+          <h3>this site is currently under construction...</h3>
+          <h3>
+            However, you can still view my <a href="/blog">Blog</a>
+            <br />
+            or follow me on{" "}
+            <a href="https://www.twitter.com/thecodepixi">Twitter</a> for
+            regular updates.
+          </h3>
+          <h3>
+            Thanks for stopping by!{" "}
+            <span role="img" aria-label="waving hand emoji">
+              👋
+            </span>
+          </h3>
+        </div>
       </div>
     </div>
   )
