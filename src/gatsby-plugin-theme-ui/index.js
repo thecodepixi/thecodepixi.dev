fbf479cfe8e42e11f1c94b589eb9b9f1d055a125
Emily Harber <emily.a.harber@gmail.com>
2020-06-01 21:20:19 -0400

    remove theme customizations

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,22 +1,22 @@
 import baseTheme from "gatsby-theme-blog/src/gatsby-plugin-theme-ui"
 
 export default {
-  ...baseTheme,
-  colors: {
-    ...baseTheme.colors,
-    text: "#333",
-    background: "linear-gradient(130deg, #e7f1f2, #bfc7d0)",
-    primary: "teal",
-    // modes: {
-    //   dark: {
-    //     background: "#333",
-    //     text: "ghostwhite",
-    //     primary: "turquoise",
-    //   },
-    // },
-  },
-  fonts: {
-    body: "Montserrat, sans-serif",
-    heading: "Raleway, sans-serif",
-  },
+  // ...baseTheme,
+  // colors: {
+  //   ...baseTheme.colors,
+  //   text: "#333",
+  //   background: "linear-gradient(130deg, #e7f1f2, #bfc7d0)",
+  //   primary: "teal",
+  //   // modes: {
+  //   //   dark: {
+  //   //     background: "#333",
+  //   //     text: "ghostwhite",
+  //   //     primary: "turquoise",
+  //   //   },
+  //   // },
+  // },
+  // fonts: {
+  //   body: "Montserrat, sans-serif",
+  //   heading: "Raleway, sans-serif",
+  // },
 }
