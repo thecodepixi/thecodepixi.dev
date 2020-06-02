2e3aa0f76080285fb6b016cdb2bb01c1e8115e2f
Emily Harber <emily.a.harber@gmail.com>
2020-06-01 21:15:47 -0400

    remove dark mode from themeui override settings

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -7,13 +7,13 @@ export default {
     text: "#333",
     background: "linear-gradient(130deg, #e7f1f2, #bfc7d0)",
     primary: "teal",
-    modes: {
-      dark: {
-        background: "#333",
-        text: "ghostwhite",
-        primary: "turquoise",
-      },
-    },
+    // modes: {
+    //   dark: {
+    //     background: "#333",
+    //     text: "ghostwhite",
+    //     primary: "turquoise",
+    //   },
+    // },
   },
   fonts: {
     body: "Montserrat, sans-serif",
