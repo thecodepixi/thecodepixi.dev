a1051d5165bd455b301953aa423e4692216eb6ed
Emily Harber <emily.a.harber@gmail.com>
2020-06-05 14:39:52 -0400

    FeaturedPosts component working! with help from PartyCorgi. Used map instead of for...of on data set

--- src/components/FeaturedPosts.js
+++ src/components/FeaturedPosts.js
@@ -18,24 +18,22 @@ export default () => {
     }
   `)
 
-  const createPosts = () => {
-    for (let post of data.allBlogPost.edges) {
-      return (
-        <section className="featured-post">
-          <Styled.h3 as={Link} href={post.node.slug}>
-            {post.node.title}
-          </Styled.h3>
-          <Styled.p>{post.node.date}</Styled.p>
-          <Styled.p>{post.node.excerpt}</Styled.p>
-        </section>
-      )
-    }
-  }
+  const createPostNodes = data.allBlogPost.edges.map(post => {
+    return (
+      <section className="featured-post">
+        <Styled.h3 as={Link} href={post.node.slug}>
+          {post.node.title}
+        </Styled.h3>
+        <Styled.p>{post.node.date}</Styled.p>
+        <Styled.p>{post.node.excerpt}</Styled.p>
+      </section>
+    )
+  })
 
   return (
     <section id="featured-blog-posts">
       <Styled.h2>Featured Blog Posts:</Styled.h2>
-      {createPosts()}
+      {createPostNodes}
     </section>
   )
 }
